b219aed683ef46d23f8cc930feee3d17b47b4ad1
thesave <Thesave86@gmail.com>
2021-06-18 15:10:19 +0200

    test for pipeline implementation

---
+++ src/test/java/PipelineQueryTest.java
@@ -0,0 +1,87 @@
+import jolie.js.JsUtils;
+import jolie.runtime.FaultException;
+import jolie.runtime.Value;
+import jolie.runtime.ValueVector;
+import joliex.tquery.engine.common.TQueryExpression;
+import joliex.tquery.engine.common.Utils;
+import joliex.tquery.engine.pipeline.PipelineQuery;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class PipelineQueryTest {
+
+	public static void main( String[] args ) throws IOException {
+		sleeplogQuery();
+	}
+
+	public static ValueVector biometricQuery() throws IOException {
+		String jsonString = Files.readString( Path.of( "tests/data/biometric-0.json" ) );
+		StringReader reader = new StringReader( jsonString );
+		Value data = Value.create();
+		JsUtils.parseJsonIntoValue( reader, data, false );
+
+		jsonString = Files.readString( Path.of( "tests/data/biometric-query.json" ) );
+		jsonString = jsonString.replaceAll( "//.+", "" );
+		reader = new StringReader( jsonString );
+		Value pipeline = Value.create();
+		JsUtils.parseJsonIntoValue( reader, pipeline, false );
+
+		Value request = Value.create();
+		request.children().put( "data", data.getChildren( "_" ) );
+		request.children().put( "pipeline", pipeline.getChildren( "_" ) );
+
+//		System.out.println( Utils.valueToPrettyString( request ) );
+
+		try {
+			Value response = PipelineQuery.pipeline( request );
+//			System.out.println( Utils.valueToPrettyString( response ) );
+			return response.getChildren( TQueryExpression.ResponseType.RESULT );
+		} catch ( FaultException e ) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void sleeplogQuery() throws IOException {
+
+		String jsonString = Files.readString( Path.of( "tests/data/sleeplog-0.json" ) );
+		StringReader reader = new StringReader( jsonString );
+		Value data = Value.create();
+		JsUtils.parseJsonIntoValue( reader, data, false );
+
+		jsonString = Files.readString( Path.of( "tests/data/sleeplog-query.json" ) );
+		jsonString = jsonString.replaceAll( "//.+", "" );
+		reader = new StringReader( jsonString );
+		Value pipeline = Value.create();
+		JsUtils.parseJsonIntoValue( reader, pipeline, false );
+
+		Value request = Value.create();
+		request.children().put( "data", data.getChildren( "_" ) );
+		request.children().put( "pipeline", pipeline.getChildren( "_" ) );
+
+		request.getChildren( "pipeline" ).stream()
+						.filter( value -> value.hasChildren( "lookupQuery" ) )
+						.findAny().ifPresent( value -> {
+			try {
+				value.getFirstChild( "lookupQuery" ).children().put( "rightData", biometricQuery() );
+			} catch ( IOException e ) {
+				e.printStackTrace();
+			}
+		} );
+
+//		System.out.println( Utils.valueToPrettyString( request ) );
+
+		try {
+			Value response = PipelineQuery.pipeline( request );
+			System.out.println( Utils.valueToPrettyString( response ) );
+		} catch ( FaultException e ) {
+			e.printStackTrace();
+		}
+
+
+	}
+
+}
